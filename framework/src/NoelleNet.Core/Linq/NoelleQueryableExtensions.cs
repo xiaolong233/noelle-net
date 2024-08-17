9d21004fc6f36ca218434a9b1bd02fb2019887df
xiaolong233 <yonglongchen@outlook.com>
2024-08-18 00:46:44 +0800

    1、WhereIf增加System.Linq.Dynamic.Core的支持；

--- framework/src/NoelleNet.Core/Linq/NoelleQueryableExtensions.cs
+++ framework/src/NoelleNet.Core/Linq/NoelleQueryableExtensions.cs
@@ -23,6 +23,37 @@ public static class NoelleQueryableExtensions
     }
 
     /// <summary>
+    /// 只有在 condition 为 true 时，才会根据 predicate 进行过滤
+    /// </summary>
+    /// <typeparam name="T">泛型类型</typeparam>
+    /// <param name="source">源 <see cref="IQueryable"/> 实例</param>
+    /// <param name="condition">条件</param>
+    /// <param name="predicate">表达式字符串</param>
+    /// <param name="args">表达式的参数</param>
+    /// <returns></returns>
+    public static IQueryable<T> WhereIf<T>(this IQueryable<T> source, bool condition, string predicate, params object?[] args)
+    {
+        return WhereIf(source, condition, Dynamic.Core.ParsingConfig.Default, predicate, args);
+    }
+
+    /// <summary>
+    /// 只有在 condition 为 true 时，才会根据 predicate 进行过滤
+    /// </summary>
+    /// <typeparam name="T">泛型类型</typeparam>
+    /// <param name="source">源 <see cref="IQueryable"/> 实例</param>
+    /// <param name="condition">条件</param>
+    /// <param name="config"><see cref="Dynamic.Core.ParsingConfig"/> 的实例</param>
+    /// <param name="predicate">表达式字符串</param>
+    /// <param name="args">表达式的参数</param>
+    /// <returns></returns>
+    public static IQueryable<T> WhereIf<T>(this IQueryable<T> source, bool condition, Dynamic.Core.ParsingConfig config, string predicate, params object?[] args)
+    {
+        ArgumentNullException.ThrowIfNull(source);
+
+        return condition ? Dynamic.Core.DynamicQueryableExtensions.Where<T>(source, config, predicate, args) : source;
+    }
+
+    /// <summary>
     /// 只有在 condition 为 true 时，才会根据 sort 进行排序
     /// </summary>
     /// <typeparam name="T">泛型类型</typeparam>
