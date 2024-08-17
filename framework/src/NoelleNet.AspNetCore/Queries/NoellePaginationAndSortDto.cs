32947185f23717f73956494d535299e11163b513
xiaolong233 <yonglongchen@outlook.com>
2024-08-18 00:22:26 +0800

    1、新增NoellePaginationDto类；

--- framework/src/NoelleNet.AspNetCore/Queries/NoellePaginationAndSortDto.cs
+++ framework/src/NoelleNet.AspNetCore/Queries/NoellePaginationAndSortDto.cs
@@ -3,7 +3,10 @@
 /// <summary>
 /// 分页查询和排序的参数
 /// </summary>
-/// <param name="Offset"> 跳过多少条记录 </param>
-/// <param name="Limit"> 返回多少条记录 </param>
-/// <param name="Sort"> 排序方式，格式：field asc|desc </param>
-public record NoellePaginationAndSortDto(int Offset = 0, int Limit = 10, string? Sort = default);
+public class NoellePaginationAndSortDto : NoellePaginationDto
+{
+    /// <summary>
+    /// 排序方式，格式：field1 asc|desc,field2 asc|desc
+    /// </summary>
+    public string? Sort { get; set; }
+}
---
+++ framework/src/NoelleNet.AspNetCore/Queries/NoellePaginationDto.cs
@@ -0,0 +1,17 @@
+﻿namespace NoelleNet.AspNetCore.Queries;
+
+/// <summary>
+/// 分页查询的参数
+/// </summary>
+public class NoellePaginationDto
+{
+    /// <summary>
+    /// 跳过多少条记录
+    /// </summary>
+    public int Offset { get; set; }
+
+    /// <summary>
+    /// 返回多少条记录
+    /// </summary>
+    public int Limit { get; set; } = 10;
+}
