0a7f04958d40cfeed378cd6b528055acbf7f8f3e
xiaolong233 <yonglongchen@outlook.com>
2025-07-01 17:44:31 +0800

    feat(http): 添加HttpClient日志记录实现

---
+++ framework/src/NoelleNet.Core/NoelleNet/Http/Logging/HttpClientLoggingOptions.cs
@@ -0,0 +1,50 @@
+﻿namespace NoelleNet.Http.Logging;
+
+/// <summary>
+/// HttpClient日志选项
+/// </summary>
+public class HttpClientLoggingOptions
+{
+    /// <summary>
+    /// 配置节点名称
+    /// </summary>
+    public const string HttpClientLogging = "HttpClientLogging";
+
+    /// <summary>
+    /// 记录请求体的最大长度，默认值：8192
+    /// </summary>
+    public int MaxRequestBodyLength { get; set; } = 8192;  // 8KB
+
+    /// <summary>
+    /// 记录响应体的最大长度，默认值：16384
+    /// </summary>
+    public int MaxResponseBodyLength { get; set; } = 16384; // 16KB
+
+    /// <summary>
+    /// 是否清理敏感数据
+    /// </summary>
+    public bool SanitizeSensitiveData { get; set; } = true;
+
+    /// <summary>
+    /// 敏感字段
+    /// </summary>
+    public List<string> SensitiveFields { get; set; } =
+    [
+        "password",
+        "token",
+        "creditcard",
+        "cvv",
+        "authorization"
+    ];
+
+    /// <summary>
+    /// 敏感标头
+    /// </summary>
+    public List<string> SensitiveHeaders { get; set; } =
+    [
+        "Authorization",
+        "Cookie",
+        "Set-Cookie",
+        "X-Api-Key"
+    ];
+}
