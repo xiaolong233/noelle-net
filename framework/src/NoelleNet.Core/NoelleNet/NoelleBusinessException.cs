1d8e892c559c4b7f3b4cb504427aff3e49b225e5
xiaolong233 <yonglongchen@outlook.com>
2025-07-14 16:06:26 +0800

    refactor(Exceptions): 重构NoelleBusinessException的构造函数

--- framework/src/NoelleNet.Core/NoelleNet/NoelleBusinessException.cs
+++ framework/src/NoelleNet.Core/NoelleNet/NoelleBusinessException.cs
@@ -5,13 +5,53 @@ namespace NoelleNet;
 /// <summary>
 /// 业务异常类，继承自 <see cref="Exception"/>，并实现 <see cref="IHasErrorCode"/> 接口。
 /// </summary>
-/// <param name="errorCode">错误代码</param>
-/// <param name="message">异常消息</param>
-/// <param name="innerException">内部异常对象</param>
-public class NoelleBusinessException(string? errorCode = default, string? message = default, Exception? innerException = default) : Exception(message, innerException), IHasErrorCode
+public class NoelleBusinessException : Exception, IHasErrorCode
 {
     /// <summary>
-    /// 获取或设置错误代码
+    /// 创建一个新的 <see cref="NoelleBusinessException"/> 实例
     /// </summary>
-    public string? ErrorCode { get; set; } = errorCode;
+    public NoelleBusinessException()
+    {
+    }
+
+    /// <summary>
+    /// 创建一个新的 <see cref="NoelleBusinessException"/> 实例
+    /// </summary>
+    /// <param name="message">错误信息</param>
+    public NoelleBusinessException(string message) : base(message)
+    {
+    }
+
+    /// <summary>
+    /// 创建一个新的 <see cref="NoelleBusinessException"/> 实例
+    /// </summary>
+    /// <param name="message">错误信息</param>
+    /// <param name="innerException">内部异常实例</param>
+    public NoelleBusinessException(string message, Exception? innerException) : base(message, innerException)
+    {
+    }
+
+    /// <summary>
+    /// 创建一个新的 <see cref="NoelleBusinessException"/> 实例
+    /// </summary>
+    /// <param name="errorCode">错误代码</param>
+    /// <param name="message">错误信息</param>
+    public NoelleBusinessException(string errorCode, string? message) : base(message)
+    {
+        ErrorCode = errorCode;
+    }
+
+    /// <summary>
+    /// 创建一个新的 <see cref="NoelleBusinessException"/> 实例
+    /// </summary>
+    /// <param name="errorCode">错误代码</param>
+    /// <param name="message">错误信息</param>
+    /// <param name="innerException">内部异常实例</param>
+    public NoelleBusinessException(string errorCode, string? message, Exception? innerException) : base(message, innerException)
+    {
+        ErrorCode = errorCode;
+    }
+
+    /// <inheritdoc/>
+    public string? ErrorCode { get; set; }
 }
