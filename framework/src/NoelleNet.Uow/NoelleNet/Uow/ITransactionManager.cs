c823d024d7ff51d4d7e0d6f56cfd823232149883
xiaolong233 <yonglongchen@outlook.com>
2025-08-05 05:16:36 +0800

    feat(uow): 添加工作单元的核心接口

---
+++ framework/src/NoelleNet.Uow/NoelleNet/Uow/ITransactionManager.cs
@@ -0,0 +1,38 @@
+﻿namespace NoelleNet.Uow;
+
+/// <summary>
+/// 事务管理器接口
+/// </summary>
+public interface ITransactionManager : IDisposable, IAsyncDisposable
+{
+    /// <summary>
+    /// 是否存在已激活的事务
+    /// </summary>
+    bool HasActiveTransaction { get; }
+
+    /// <summary>
+    /// 事务标识
+    /// </summary>
+    Guid? TransactionId { get; }
+
+    /// <summary>
+    /// 启用一个新的事务
+    /// </summary>
+    /// <param name="cancellationToken">传播取消操作的通知</param>
+    /// <returns></returns>
+    Task BeginAsync(CancellationToken cancellationToken = default);
+
+    /// <summary>
+    /// 提交对当前事务中数据库所做的所有更改
+    /// </summary>
+    /// <param name="cancellationToken">传播取消操作的通知</param>
+    /// <returns></returns>
+    Task CommitAsync(CancellationToken cancellationToken = default);
+
+    /// <summary>
+    /// 放弃对当前事务中数据库所做的所有更改
+    /// </summary>
+    /// <param name="cancellationToken">传播取消操作的通知</param>
+    /// <returns></returns>
+    Task RollbackAsync(CancellationToken cancellationToken = default);
+}
---
+++ framework/src/NoelleNet.Uow/NoelleNet/Uow/IUnitOfWork.cs
@@ -0,0 +1,14 @@
+﻿namespace NoelleNet.Uow;
+
+/// <summary>
+/// 工作单元接口
+/// </summary>
+public interface IUnitOfWork
+{
+    /// <summary>
+    /// 将所有更改保存到数据库
+    /// </summary>
+    /// <param name="cancellationToken">传播取消操作的通知</param>
+    /// <returns></returns>
+    Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+}
