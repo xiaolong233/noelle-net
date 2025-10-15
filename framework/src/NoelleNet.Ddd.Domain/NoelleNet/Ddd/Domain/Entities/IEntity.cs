5b93819409e5f8be350cf21f3cc86c409fd4db00
xiaolong233 <yonglongchen@outlook.com>
2025-10-15 17:55:48 +0800

    refactor(Domain): 重构实体接口和基类
    支持复合主键的场景

--- framework/src/NoelleNet.Ddd.Domain/NoelleNet/Ddd/Domain/Entities/IEntity.cs
+++ framework/src/NoelleNet.Ddd.Domain/NoelleNet/Ddd/Domain/Entities/IEntity.cs
@@ -1,8 +1,25 @@
 ﻿namespace NoelleNet.Ddd.Domain.Entities;
 
 /// <summary>
-/// 实体接口
+/// 定义一个实体
 /// </summary>
 public interface IEntity
 {
+    /// <summary>
+    /// 返回实体的标识符数组
+    /// </summary>
+    /// <returns></returns>
+    object?[] GetIdentifiers();
+}
+
+/// <summary>
+/// 定义一个具有 Id 属性的单个标识符的实体
+/// </summary>
+/// <typeparam name="TIdentifier"></typeparam>
+public interface IEntity<TIdentifier> : IEntity
+{
+    /// <summary>
+    /// 实体的标识符
+    /// </summary>
+    TIdentifier Id { get; }
 }
