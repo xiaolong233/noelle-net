acd64db3d062c404bb88c5615824436abd78def2
xiaolong233 <yonglongchen@outlook.com>
2026-01-31 19:07:12 +0800

    refactor: 重构业务异常类的构造函数

--- framework/src/NoelleNet.Core/NoelleNet/BusinessException.cs
+++ framework/src/NoelleNet.Core/NoelleNet/BusinessException.cs
@@ -5,57 +5,36 @@ using NoelleNet.Logging;
 namespace NoelleNet;
 
 /// <summary>
-/// 业务异常类，继承自 <see cref="Exception"/>，并实现 <see cref="IHasErrorCode"/> 接口。
+/// 业务异常类
 /// </summary>
-public class BusinessException : Exception, IBusinessException, IHasErrorCode, IHasLogLevel
+public class BusinessException : Exception, IBusinessException, IHasErrorCode, IHasErrorDetails, IHasLogLevel
 {
     /// <summary>
     /// 创建一个新的 <see cref="BusinessException"/> 实例
     /// </summary>
-    public BusinessException()
-    {
-    }
-
-    /// <summary>
-    /// 创建一个新的 <see cref="BusinessException"/> 实例
-    /// </summary>
-    /// <param name="message">错误信息</param>
-    public BusinessException(string message) : base(message)
-    {
-    }
-
-    /// <summary>
-    /// 创建一个新的 <see cref="BusinessException"/> 实例
-    /// </summary>
-    /// <param name="message">错误信息</param>
-    /// <param name="innerException">内部异常实例</param>
-    public BusinessException(string message, Exception? innerException) : base(message, innerException)
-    {
-    }
-
-    /// <summary>
-    /// 创建一个新的 <see cref="BusinessException"/> 实例
-    /// </summary>
     /// <param name="errorCode">错误代码</param>
     /// <param name="message">错误信息</param>
-    public BusinessException(string errorCode, string? message) : base(message)
+    /// <param name="details">错误详情</param>
+    /// <param name="innerException">内部异常</param>
+    /// <param name="logLevel">日志等级</param>
+    public BusinessException(
+        string? errorCode = null,
+        string? message = null,
+        string? details = null,
+        Exception? innerException = null,
+        LogLevel logLevel = LogLevel.Warning)
+        : base(message, innerException)
     {
-        ErrorCode = errorCode;
-    }
-
-    /// <summary>
-    /// 创建一个新的 <see cref="BusinessException"/> 实例
-    /// </summary>
-    /// <param name="errorCode">错误代码</param>
-    /// <param name="message">错误信息</param>
-    /// <param name="innerException">内部异常实例</param>
-    public BusinessException(string errorCode, string? message, Exception? innerException) : base(message, innerException)
-    {
-        ErrorCode = errorCode;
+        this.ErrorCode = errorCode;
+        this.Details = details;
+        this.LogLevel = logLevel;
     }
 
     /// <inheritdoc/>
     public string? ErrorCode { get; set; }
+
+    /// <inheritdoc/>
+    public string? Details { get; set; }
 
     /// <inheritdoc/>
     public LogLevel LogLevel { get; set; } = LogLevel.Warning;
