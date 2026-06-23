992d11aa8a8cf474b7eb6b13c1c292d03071e7d4
xiaolong233 <yonglongchen@outlook.com>
2026-06-24 04:05:02 +0800

    refactor: 优化异常处理过滤器，使用DI注入日志记录器和结构化日志

--- framework/src/NoelleNet.AspNetCore/NoelleNet/AspNetCore/ExceptionHandling/NoelleExceptionHandlingFilter.cs
+++ framework/src/NoelleNet.AspNetCore/NoelleNet/AspNetCore/ExceptionHandling/NoelleExceptionHandlingFilter.cs
@@ -4,8 +4,6 @@ using Microsoft.Extensions.Options;
 using NoelleNet.Http;
 using NoelleNet.Logging;
 using System.Net;
-using System.Text;
-using System.Text.Json;
 
 namespace NoelleNet.AspNetCore.ExceptionHandling;
 
@@ -14,39 +12,33 @@ namespace NoelleNet.AspNetCore.ExceptionHandling;
 /// </summary>
 public class NoelleExceptionHandlingFilter : IAsyncExceptionFilter
 {
+    private readonly ILogger<NoelleExceptionHandlingFilter> _logger;
     private readonly IExceptionToErrorConverter _converter;
     private readonly IHttpExceptionStatusCodeFinder _finder;
     private readonly IOptions<NoelleExceptionHandlingOptions> _exceptionHandlingOptions;
-    private readonly JsonSerializerOptions _serializerOptions;
 
     /// <summary>
     /// 创建一个新的 <see cref="NoelleExceptionHandlingFilter"/> 实例
     /// </summary>
+    /// <param name="logger">日志记录器</param>
     /// <param name="converter">异常信息转换器</param>
     /// <param name="finder">HTTP错误状态码查找器</param>
     /// <param name="exceptionHandlingOptions">异常处理选项</param>
     /// <exception cref="ArgumentNullException"></exception>
-    public NoelleExceptionHandlingFilter(IExceptionToErrorConverter converter, IHttpExceptionStatusCodeFinder finder, IOptions<NoelleExceptionHandlingOptions> exceptionHandlingOptions)
+    public NoelleExceptionHandlingFilter(ILogger<NoelleExceptionHandlingFilter> logger, IExceptionToErrorConverter converter, IHttpExceptionStatusCodeFinder finder, IOptions<NoelleExceptionHandlingOptions> exceptionHandlingOptions)
     {
+        _logger = logger ?? throw new ArgumentNullException(nameof(logger));
         _converter = converter ?? throw new ArgumentNullException(nameof(converter));
         _finder = finder ?? throw new ArgumentNullException(nameof(finder));
         _exceptionHandlingOptions = exceptionHandlingOptions ?? throw new ArgumentNullException(nameof(exceptionHandlingOptions));
-
-        _serializerOptions = new JsonSerializerOptions(JsonSerializerDefaults.General);
-        _serializerOptions.WriteIndented = true; // 格式化输出
     }
 
-    /// <summary>
-    /// 在操作引发 <see cref="Exception"/> 后调用 
-    /// </summary>
-    /// <param name="context">异常上下文</param>
-    /// <returns></returns>
+    /// <inheritdoc/>
     public Task OnExceptionAsync(ExceptionContext context)
     {
         if (context.ExceptionHandled)
             return Task.CompletedTask;
 
-        // 把异常对象转换成错误信息对象
         var error = _converter.Covert(context.Exception, options =>
         {
             options.TraceIdProvider = _exceptionHandlingOptions.Value.TraceIdProvider ?? (() => context.HttpContext.TraceIdentifier);
@@ -55,30 +47,22 @@ public class NoelleExceptionHandlingFilter : IAsyncExceptionFilter
             options.IncludeExceptionData = _exceptionHandlingOptions.Value.IncludeExceptionData;
         });
 
-        // 根据异常获取对应的HTTP状态码
         HttpStatusCode statusCode = _finder.GetStatusCode(context.HttpContext, context.Exception);
 
-        // 记录日志信息
-        ILogger<NoelleExceptionHandlingFilter> logger = context.HttpContext.RequestServices.GetRequiredService<ILogger<NoelleExceptionHandlingFilter>>();
-        var logLevel = LogLevel.Error;
-        if (context.Exception is IHasLogLevel hasLogLevel)
+        LogLevel logLevel = context.Exception switch
         {
-            logLevel = hasLogLevel.LogLevel;
-        }
-        else if (context.Exception is IBusinessException)
+            IHasLogLevel hasLogLevel => hasLogLevel.LogLevel,
+            IBusinessException => LogLevel.Information,
+            _ => LogLevel.Error
+        };
+
+        _logger.Log(logLevel, context.Exception, "{@ErrorInfo}", error);
+
+        context.Result = new JsonResult(new ErrorResponseDto(error))
         {
-            logLevel = LogLevel.Information;
-        }
+            StatusCode = (int)statusCode
+        };
 
-        StringBuilder messageBuilder = new StringBuilder();
-        messageBuilder.AppendLine($"============ {nameof(RemoteCallErrorInfo)} ============");
-        messageBuilder.AppendLine(JsonSerializer.Serialize(error, _serializerOptions));
-        logger.Log(logLevel, context.Exception, message: messageBuilder.ToString(), []);
-
-        context.HttpContext.Response.StatusCode = (int)statusCode;
-        context.Result = new JsonResult(new ErrorResponseDto(error));
-
-        // 标记异常已被处理
         context.ExceptionHandled = true;
 
         return Task.CompletedTask;
